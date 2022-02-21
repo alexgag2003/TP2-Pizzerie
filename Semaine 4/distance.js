1c9e55a663f3780b9501683732c961f5461f9cce
alexgag2003 <79212441+alexgag2003@users.noreply.github.com>
2022-02-21 17:23:00 -0500

    yo

--- Semaine 4/distance.js
+++ Semaine 4/distance.js
@@ -4,12 +4,15 @@ class Point{
         this.coordY = coordY;
     }
 
-
+    distance(autrePoint){
+        return Math.sqrt((autrePoint.coordX - this.coordY)**2 + (autrePoint.coordY - this.coordY)**2);
+    }
 }
 
 var point1 = new Point(2,2);
 var point2 = new Point(5,6);
  
 
-console.log(point1);
+console.log("distance entre 2 points: " + point1.distance(point2));
 console.log(point2);
+//console.log(point1);
