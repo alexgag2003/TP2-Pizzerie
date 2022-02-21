82d3769c1961d01e29a4e413858bc77da6200917
alexgag2003 <79212441+alexgag2003@users.noreply.github.com>
2022-02-21 17:43:54 -0500

    Correction

--- Semaine 4/distance.js
+++ Semaine 4/distance.js
@@ -16,3 +16,22 @@ var point2 = new Point(5,6);
 console.log("distance entre 2 points: " + point1.distance(point2));
 console.log(point2);
 //console.log(point1);
+
+class Cercle{
+    constructor(point, rayon){
+        this.point=point;
+        this.rayon=rayon;
+    }
+
+
+        perimetre(){
+            return 2 * Math.PI *this.rayon;
+        }
+
+        aire(){
+            return Math.PI * (this.rayon **2);
+        }
+}
+var point3 = new Point(2,2);
+var cercle = new Cercle(point1);
+console.log("perimetre" + cercle.perimetre);
