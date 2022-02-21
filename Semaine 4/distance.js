dfbfcb816beadfda04aa4cf432bf67211e238de6
alexgag2003 <alexgag2003@outlook.fr>
2022-02-21 17:10:41 -0500

    lol

---
+++ Semaine 4/distance.js
@@ -0,0 +1,15 @@
+class Point{
+    constructor(coordX,coordY){
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+
+
+}
+
+var point1 = new Point(2,2);
+var point2 = new Point(5,6);
+ 
+
+console.log(point1);
+console.log(point2);
